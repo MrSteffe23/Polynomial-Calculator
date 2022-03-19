072edaf10b6483f9436246bafab309a663d8642c
Atitienei Stefan <stef_stefan9@yahoo.com>
2022-03-19 16:19:06 +0200

    first commit

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        PolView view = new PolView();
-        PolModel model = new PolModel();
-        new PolController(model, view);
-        view.setVisible(true);
-    }
-}
